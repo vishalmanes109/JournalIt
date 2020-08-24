f732e6188b03824e89457fac9b751bacfa723bb6
vishalmanes109 <36571896+vishalmanes109@users.noreply.github.com>
2020-08-24 21:22:17 +0530

    for heroku deploy

--- server.js
+++ server.js
@@ -1,8 +1,8 @@
 const express = require("express");
 const path = require("path");
 const app = express();
-app.use(express.static(__dirname + "/dist/journal-it"));
+app.use(express.static(__dirname + "/dist/JournalIT"));
 app.get("/*", function (req, res) {
-  res.sendFile(path.join(__dirname + "/dist/journal-it/index.html"));
+  res.sendFile(path.join(__dirname + "/dist/JournalIT/index.html"));
 });
 app.listen(process.env.PORT || 8080);
